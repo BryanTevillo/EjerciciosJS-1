64e296fee1e494bcdebfc8d6e26917d64de4064c
BryanTevillo <tevillo24@gmail.com>
2024-01-08 16:43:58 -0600

    Se agrega descripcion de los ejercicios y se agregan 10 ejemplos tipo numerico

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log("hola mundo");
+/** Ejercicio para esta sesion
+ *
+ * Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
+ * Pueden variar entre las palabras reservadas var, let y const
+ *      -number
+ *      -string
+ *      -boolean
+ */
+
+//Numericas
+var edad = 25;
+var temperatura = 30.5;
+var deuda = -2000;
+var cantidadProductos = 100;
+var hexadecimal = 0xff;
+let notacionExponencial = 42e3;
+let ejemploEntero = 100000000;
+const numeroAureo = 1.618;
+const pi = 3.1416;
+const piNegativo = -3.1426;
+//Representacion en consola :D
+console.log(edad);
+console.log(temperatura);
+console.log(deuda);
+console.log(cantidadProductos);
+console.log(hexadecimal);
+console.log(notacionExponencial);
+console.log(ejemploEntero);
+console.log(numeroAureo);
+console.log(pi);
+console.log(piNegativo);
+
+/*
+ * Declarar variables numericas y realizar las operaciones aritmeticas basicas
+ *      -suma
+ *      -resta
+ *      -multiplicacion
+ *      -division
+ *
+ * Declarar variables que concatenen cadenas de texto
+ *      -5 mensajes diferentes en pantalla, sin limite de palabras.
+ *
+ * Declarar 5 variables y convertirlas a otro tipo de dato
+ *
+ *  CONSIDERACIONES: poner en practica las mejores practicas para declaracion de variables.
+ * Imprimir los resultados en pantalla con un console.log()
+ */
