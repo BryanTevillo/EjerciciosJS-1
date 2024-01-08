9a0b4d1e5eb7418775fec2938a0a34731b912002
BryanTevillo <tevillo24@gmail.com>
2024-01-08 16:54:50 -0600

    Operaciones aritmeticas

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ let ejemploEntero = 100000000;
 const numeroAureo = 1.618;
 const pi = 3.1416;
 const piNegativo = -3.1426;
-//Representacion en consola :D
+//Representacion en consola  numerico:D
 console.log(edad);
 console.log(temperatura);
 console.log(deuda);
@@ -41,7 +41,8 @@ const cadenaLarga = "Una cadena más larga para demostrar longitud";
 const cadenaVacia = "";
 const cadenaNumerica = "12345";
 const cadenaBooleana = "true";
-//Representacion en consola
+
+//Representacion en consola String
 console.log(saludo);
 console.log(despedida);
 console.log(plantilla);
@@ -52,6 +53,32 @@ console.log(cadenaLarga);
 console.log(cadenaVacia);
 console.log(cadenaNumerica);
 console.log(cadenaBooleana);
+
+//Bopleano
+var esDia = true;
+var esNoche = false;
+var esMayor = 10 > 5;
+let esIgual = 3 === 5;
+let esMenor = 1 < 0;
+const esMayorIgual = 5 >= 5;
+const noEsIgual = 100 !== "100";
+const yLogico = true && false;
+const oLogico = false || true;
+const negacion = !esDia;
+
+//Representacion en consola boolean
+
+console.log(esDia);
+console.log(esNoche);
+console.log(esMayor);
+console.log(esIgual);
+console.log(esMenor);
+console.log(esMayorIgual);
+console.log(noEsIgual);
+console.log(yLogico);
+console.log(oLogico);
+console.log(negacion);
+
 /*
  * Declarar variables numericas y realizar las operaciones aritmeticas basicas
  *      -suma
@@ -59,6 +86,26 @@ console.log(cadenaBooleana);
  *      -multiplicacion
  *      -division
  *
+ */
+//Operaciones aritmeticas con las variables numericas
+
+//SUMA
+const suma = edad + numeroAureo;
+console.log(suma);
+
+//RESTA
+const resta = pi - numeroAureo;
+console.log(resta);
+
+//MULTIPLICACION
+const multiplicacion = temperatura * deuda;
+console.log(multiplicacion);
+
+//DIVISIOM
+const division = edad / pi;
+console.log(division);
+
+/*
  * Declarar variables que concatenen cadenas de texto
  *      -5 mensajes diferentes en pantalla, sin limite de palabras.
  *
