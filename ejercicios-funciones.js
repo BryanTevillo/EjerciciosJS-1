b9fd66f6e6506c536e2c07537dd7d1145ea72e1d
BryanTevillo <tevillo24@gmail.com>
2024-01-09 13:19:09 -0600

    Se realizan las funciones flechas para los ejercicios y se comprueba resultados por consola se termina ejercicios

--- ejercicios-funciones.js
+++ ejercicios-funciones.js
@@ -28,7 +28,7 @@ function areaRectangulo(largo, alto) {
 
 //! funcion cuadrado de un numero
 function cuadradoNumero(numero) {
-    return `El cuadrado del numero es: ${numero ** 2}`;
+    return `El cuadrado del numero ${numero} es: ${numero ** 2}`;
 }
 
 //!funcion  conversion de grados celsius a farenheit
@@ -45,9 +45,29 @@ function volumenEsfera(radio) {
     return `El volumen de la esfera es: ${(radio ** 3 * 4 * pi) / 3}`;
 }
 
+console.log("********Resultados funciones con nombre**************");
+//? impresion por consola de las funciones con nombre
 console.log(perimetroCirculo(3));
 console.log(areaRectangulo(10, 5));
 console.log(cuadradoNumero(5));
 console.log(celciusFarenheit(60));
 console.log(voltajeOhm(500, 1));
 console.log(volumenEsfera(8));
+
+//*************** Funciones flecha realizando los ejercicios ***********************
+
+const flechaPerimetroCirculo = (radio) => `El perimetro del circulo es: ${2 * pi * radio}`;
+const flechaAreaRectangulo = (largo, alto) => `El area del rectangulo es: ${largo * alto}`;
+const flechaCuadradoNumero = (numero) => `El cuadrado del numero ${numero} es: ${numero ** 2}`;
+const flechaCelciusFarenheit = (celcius) => `La conversion de ${celcius} grados celcius a Farenheit es: ${celcius * 1.8 + 32} `;
+const flechaVoltajeOhm = (resistencia, corriente) => `El voltaje de la resistencia es : ${resistencia * corriente}`;
+const flechaVolumenEsfera = (radio) => `El volumen de la esfera es: ${(radio ** 3 * 4 * pi) / 3}`;
+
+console.log("***********************Resultados funciones flecha*******************");
+//? impresion por consola de las funciones con nombre
+console.log(flechaPerimetroCirculo(3));
+console.log(flechaAreaRectangulo(10, 5));
+console.log(flechaCuadradoNumero(5));
+console.log(flechaCelciusFarenheit(60));
+console.log(flechaVoltajeOhm(500, 1));
+console.log(flechaVolumenEsfera(8));
